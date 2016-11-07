7a0fdc0c695432c0c509363103188854e6503523
Viraj Parab <virajpa@thoughtworks.com>
2016-11-07 20:17:16 +0530

    refactor excercise 4

--- public/javascript/exercise4.js
+++ public/javascript/exercise4.js
@@ -1,18 +1,9 @@
-
-var getValues =  function (startingValue,operation) {
-    var data = [startingValue];
-    for(var i = 1; i <= 10; i++){
-        data.push(operation(i));
-    };
-    return data;
-};
-
-var generateRow = function (table,data,rowType) {
+var generateRow = function (table,data,scale,rowType) {
     table.append('tr')
         .selectAll(rowType)
         .data(data).enter()
         .append(rowType)
-        .text(function(d) { return d; });
+        .text(function(d) { return scale(d); });
 };
 
 var noOperation = function (x) {
@@ -25,12 +16,14 @@ var roundFigOfLog = function (x) {
 }
 
 var generateChart = function () {
+    var data = [1,2,3,4,5,6,7,8,9,10];
+
     var table = d3.select('.chart').append('table');
-    generateRow(table,getValues("Title",noOperation),'th');
-    generateRow(table,getValues("n",noOperation),'td');
-    generateRow(table,getValues("n square",d3.scalePow().exponent(2)),'td');
-    generateRow(table,getValues("log(n)",d3.scaleLog().base(Math.E)),'td');
-    generateRow(table,getValues("log(n) rounded",roundFigOfLog),'td');
+    generateRow(table,data,noOperation,'th');
+    generateRow(table,data,noOperation,'td');
+    generateRow(table,data,d3.scalePow().exponent(2),'td');
+    generateRow(table,data,d3.scaleLog().base(Math.E),'td');
+    generateRow(table,data,roundFigOfLog,'td');
 };
 
 window.onload = function() {
