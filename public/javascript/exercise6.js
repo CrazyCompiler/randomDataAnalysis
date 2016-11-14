07873f067e18d5e2139c0adbcda0f142906dbe64
Viraj Parab <virajpa@thoughtworks.com>
2016-11-14 23:50:02 +0530

    excecise 6 in peogress

---
+++ public/javascript/exercise6.js
@@ -0,0 +1,56 @@
+const WIDTH = 600;
+const HEIGHT = 100;
+const figureHeight = 100;
+const figureWidth = 100;
+const INITIAL = 0;
+
+var generateFigures = function (figuresConfigurations) {
+
+}
+
+var generateChart = function () {
+    var svg = d3.select('.chart')
+                .append('svg')
+                .attr('width', WIDTH)
+                .attr('height', HEIGHT);
+
+
+    // var line = {
+    //     'type':'line',
+    //     'x1'  :figureWidth,
+    //     'y1'  :INITIAL,
+    //     'x2'  :INITIAL,
+    //     'y2'  :figureHeight
+    // };
+    //
+
+    svg.append('line')
+        .attr('x1', figureWidth)
+        .attr('y1', INITIAL)
+        .attr('x2', INITIAL)
+        .attr('y2', figureHeight)
+        .attr('stroke','brown')
+        .attr('stroke-width',2);
+
+    svg.append('circle')
+        .attr('cx',figureWidth*2)
+        .attr('cy',HEIGHT/2)
+        .attr('r',HEIGHT/2)
+        .attr('fill','none')
+        .attr('stroke','red')
+        .attr('stroke-width',2);
+
+    svg.append('rect')
+        .attr("x", figureWidth*3)
+        .attr("y", INITIAL)
+        .attr("width", figureWidth)
+        .attr("height", figureHeight)
+        .attr('fill','none')
+        .attr('stroke','blue')
+        .attr('stroke-width',2);
+
+
+
+};
+
+window.onload = generateChart();
